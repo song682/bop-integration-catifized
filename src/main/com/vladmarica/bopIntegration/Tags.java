3be2cbccfdf17ebb0d5e00be89c865f3491ef65a
dfdvdsf <81430928+song682@users.noreply.github.com>
2025-11-26 23:17:08 +0800

    Delete src/main/com/vladmarica/bopIntegration directory

--- src/main/com/vladmarica/bopIntegration/Tags.java
+++
@@ -1,7 +0,0 @@
-package com.vladmarica.bopIntegration;
-
-public class Tags {
-    static final String MODID = "BOPIntegrations";
-    static final String MODNAME = "BOPIntegrations";
-    static final String VERSION =  "1.6.0.5-Catized";
-}
